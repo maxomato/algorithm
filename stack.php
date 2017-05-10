dd8f0c8da28800bc1bd9b71b4173341cc558521d
Maxim Popov <maxomato@gmail.com>
2017-05-10 06:48:02 +0000

    stack: incr, decr, test function

--- stack.php
+++ stack.php
@@ -13,34 +13,38 @@ class Stack {
     }
 
     public function push($x) {
-        if ($this->sp + 1 > $this->n - 1) 
+        if ($this->sp + 1 > $this->n) 
             return false;
         
-        $this->sp = $this->sp + 1;
-        return $this->s[ $this->sp ] = $x;
+        return $this->s[ ++$this->sp ] = $x;
     }
 
     public function pop() {
         if ($this->sp < 1)
             return false;
 
-        $pop = $this->s[ $this->sp ];
-        $this->sp = $this->sp - 1;
-        return $pop;
+        return $this->s[ $this->sp-- ];
     }
 
 };
 
+function test($stack) {
+    echo $stack->push(2) . "\n";
+    echo $stack->push(3) . "\n";
+    echo $stack->push(5) . "\n";
+    echo $stack->pop() . "\n";
+    echo $stack->pop() . "\n";
+    echo $stack->pop() . "\n";
+    echo $stack->pop() . "\n";    
+}
 
-$stack = new Stack(10);
+test( new Stack(10) );
+test( new Stack(2) );
 
-echo $stack->push(2) . "\n";
-echo $stack->push(3) . "\n";
-echo $stack->push(5) . "\n";
-echo $stack->pop() . "\n";
-echo $stack->pop() . "\n";
-echo $stack->pop() . "\n";
-echo $stack->pop() . "\n";
+
+
+
+
 
 
 
